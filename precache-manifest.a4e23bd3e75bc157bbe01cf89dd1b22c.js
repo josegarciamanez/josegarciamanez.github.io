81623d0f2a934f7149272580c10f9c6d608c581b
Jose Garcia <josegarciamanez@gmail.com>
2019-04-16 23:03:02 +0200

    cambio de template

---
+++ precache-manifest.a4e23bd3e75bc157bbe01cf89dd1b22c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "847ee8875aa1254a8cc8",
+    "url": "/static/css/main.df57359f.chunk.css"
+  },
+  {
+    "revision": "847ee8875aa1254a8cc8",
+    "url": "/static/js/main.5796dccf.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "b6aafa07ca16cd706e06",
+    "url": "/static/js/2.2752fb64.chunk.js"
+  },
+  {
+    "revision": "b72e18c4d3fc6cf3cd25a6f448576986",
+    "url": "/index.html"
+  }
+];
